429eea8f858257dd9f77dc7c94d88a7d2c862ad0
Arun <arungawande1995@gmail.com>
2022-04-07 18:17:01 +0530

    Added: Parameterized Constructor to take Mood Massage

---
+++ main/java/com/bridgelabz/moodanalyser/MoodAnalyzer.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyzer
+{
+    String message;
+
+    public MoodAnalyzer() {
+
+    }
+
+    public MoodAnalyzer(String message){
+        this.message = message;
+    }
+
+    public String analyzeMood() {
+
+        if(this.message.contains("Sad")) {
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
+}
